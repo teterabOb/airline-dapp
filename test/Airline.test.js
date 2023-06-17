ac8ab516a774a6f0f73ba4410707e19d909514d2
Gilberts Ahumada <gilbertsahumada@gmail.com>
2023-06-17 10:17:06 -0400

    Update Airline.test.js

--- test/Airline.test.js
+++ test/Airline.test.js
@@ -1,74 +1,80 @@
-const { default: Web3 } = require("web3");
+const { ethers } = require("hardhat");
+const { assert } = require("chai");
 
-const Airline = artifacts.require('Airline');
+describe("Airline", () => {
+  let instance;
+  let accounts;
 
-let instance;
+  beforeEach(async () => {
+    const Airline = await ethers.getContractFactory("Airline");
+    instance = await Airline.deploy();
+    await instance.deployed();
 
-beforeEach(async() => {
-    instance = await Airline.new();    
-});
+    accounts = await ethers.getSigners();
+  });
 
-contract('Airline', (accounts) => {
-    it('should have available flights', async() => {
-        let total = await instance.totalFlights();
-        assert(total > 0);
-    });
+  it("should have available flights", async () => {
+    let total = await instance.totalFlights();
+    assert(total > 0);
+  });
 
-    it('should allow customer to buy a flight providing its value', async() =>{
-        let flight = await instance.flights(0);
-        let flightName = flight[0], price = flight[1];
-        //console.log(flight);
-        
-        await instance.buyFlight(0, { from: accounts[0], value: price });
-        let customerFlight = await instance.customerFlights(accounts[0], 0);
-        let customerTotalFlights = await instance.customerTotalFlights(accounts[0]);
-        //console.log(customerFlight, customerTotalFlights);
+  it("should allow customer to buy a flight providing its value", async () => {
+    let flight = await instance.flights(0);
+    let flightName = flight.name;
+    let price = flight.price;
 
-        assert(customerFlight[0], flightName);
-        assert(customerFlight[1], price);
-        assert(customerTotalFlights, 1);
-    });
+    await instance.buyFlight(0, { value: price });
+    let customerFlight = await instance.customerFlights(accounts[0].address, 0);
+    let customerTotalFlights = await instance.customerTotalFlights(accounts[0].address);
 
-    it('it should now allow customers to buy flights under the price', async () => {
-        let flight = await instance.flights(0);
-        let price = flight[1] - 5000;
-        try{    
-            await instance.buyFlight(0, {from: accounts[0], value: price });
-        }
-        catch (e) { return; }
-        assert.fail();
-    });
+    console.log("customerTotalFlights.toString() : ", customerTotalFlights.toString());
+    console.log("PRICE : ", customerFlight.price);
 
-    it('it should get the real balance of the contract', async() => {
+    assert.equal(customerFlight.name, flightName);
+    assert.equal(customerFlight.price.toString(), price);
+    assert.equal(customerTotalFlights.toString(), "1");
+  });
 
-        let flight = await instance.flights(0);
-        let price = flight[1];
+  it("should not allow customers to buy flights under the price", async () => {
+    let flight = await instance.flights(0);
+    let price = flight.price - 5000;
+    try {
+      await instance.buyFlight(0, { value: price });
+    } catch (e) {
+      return;
+    }
+    assert.fail();
+  });
 
-        let flight2 = await instance.flights(1);
-        let price2 = flight2[1]; 
+  it("should get the real balance of the contract", async () => {
+    let flight = await instance.flights(0);
+    let price = flight.price;
 
-        await instance.buyFlight(0, { from: accounts[0], value: price });
-        await instance.buyFlight(1, { from: accounts[0], value: price2 });
+    let flight2 = await instance.flights(1);
+    let price2 = flight2.price;
 
-        let newAirlineBalance = await instance.getAirlineBalance();
+    await instance.buyFlight(0, { value: price });
+    await instance.buyFlight(1, { value: price2 });
 
-        assert.equal(newAirlineBalance.toNumber(), price.toNumber() + price2.toNumber());
-    });
+    let newAirlineBalance = await instance.getAirlineBalance();
 
-    it('should allow customers to redeem loyalty points', async() => {
-        let flight = await instance.flights(1);
-        let price = flight[1];
+    assert(newAirlineBalance.eq(price.add(price2)), "Incorrect balance");
+  });
 
-        await instance.buyFlight(1, {from: accounts[0], value: price });
+  it("should allow customers to redeem loyalty points", async () => {
+    let flight = await instance.flights(1);
+    let price = flight.price;
 
-        let balance = await Web3.eth.getBalance(accounts[0]);
-        await instance.redeemLoyaltyPoints({ from: accounts[0] });
-        let finalBalance = await Web3.eth.getBalance(accounts[0]);
+    await instance.buyFlight(1, { value: price });
 
-        let customer = await instance.customers(accounts[0]);
-        let loyaltyPoints = customer[0];
+    let balance = await ethers.provider.getBalance(accounts[0].address);
+    await instance.redeemLoyaltyPoints({ from: accounts[0].address });
+    let finalBalance = await ethers.provider.getBalance(accounts[0].address);
 
-        assert(loyaltyPoints, 0);
-        assert(finalBalance > balance);
-    });
+    let customer = await instance.customers(accounts[0].address);
+    let loyaltyPoints = customer.loyaltyPoints;
+
+    assert.equal(loyaltyPoints, 0);
+    assert(finalBalance > balance);
+  });
 });
